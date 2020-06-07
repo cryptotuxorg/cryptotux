fcc9f7b64910d34aa080de87fd2433890e62253c
Xavier Lavayssière <xavier@bricodeur.com>
2020-06-07 19:29:05 +0200

    Bitcoin and Lightning script

---
+++ assets/.cryptotux/scripts/bitcoin.sh
@@ -0,0 +1,74 @@
+### Bitcoin ###
+# Assumes node and regtest configuration from main script
+
+B="\033[1m"
+N="\033[0m"
+
+## Install Bitcoin source code
+mkdir -p ~/Projects/
+git clone https://github.com/bitcoin/bitcoin ~/Projects/Bitcoin
+echo -e "Bitcoin source code available at $B ~/Projects/Bitcoin$N"
+
+## Electrum wallet
+# TODO : automated release version
+electrumVersion=3.3.8
+sudo apt-get install -y --no-install-recommends python3-pyqt5 python3-setuptools python3-pip
+wget -q https://download.electrum.org/$electrumVersion/Electrum-$electrumVersion.tar.gz
+# Signature verification
+gpg --keyserver keys.gnupg.net --recv-keys 6694D8DE7BE8EE5631BED9502BD5824B7F9470E6
+wget -q https://download.electrum.org/$electrumVersion/Electrum-$electrumVersion.tar.gz.asc
+gpg --status-fd 1 --verify Electrum-$electrumVersion.tar.gz.asc Electrum-$electrumVersion.tar.gz 2>/dev/null | grep -q 'GOODSIG' || echo -e "$B Error in Electrum signature$N"
+# Install
+python3 -m pip install --user Electrum-$electrumVersion.tar.gz
+export PATH=$PATH:$HOME/.local/bin
+rm Electrum-$electrumVersion*
+echo -e "Electrum added, available via $B electrum$N and graphically"
+
+## Bitcoin local network tutorial
+cd  ~/.bitcoin/
+# Create Alice node
+mkdir -p nodeAlice/
+cp ~/.bitcoin/bitcoin.conf nodeAlice/bitcoin.conf
+echo '
+[regtest]
+bind=127.0.0.1
+port=18441
+rpcbind=127.0.0.1
+rpcport=18401
+
+connect=127.0.0.1:18442
+connect=127.0.0.1:18444
+
+' >> nodeAlice/bitcoin.conf
+sed -i 's/uacomment.*/uacomment=Alice/g' nodeAlice/bitcoin.conf
+
+# Create Bob node
+mkdir -p nodeBob/
+cp ~/.bitcoin/bitcoin.conf nodeBob/bitcoin.conf
+echo '
+[regtest]
+bind=127.0.0.1
+port=18442
+rpcbind=127.0.0.1
+rpcport=18402
+
+connect=127.0.0.1:18441
+connect=127.0.0.1:18444
+' >> nodeBob/bitcoin.conf
+sed -i 's/uacomment.*/uacomment=Bob/g' nodeBob/bitcoin.conf
+
+# Configure aliases
+echo '
+export AliceDir="$HOME/.bitcoin/nodeAlice";
+export BobDir="$HOME/.bitcoin/nodeBob";
+alias bitcoind-Alice="bitcoind -datadir=$AliceDir";
+alias bitcoin-cli-Alice="bitcoin-cli -datadir=$BobDir";
+alias bitcoind-Bob="bitcoind -datadir=$BobDir";
+alias bitcoin-cli-Bob="bitcoin-cli -datadir=$BobDir";
+' >> ~/.bashrc
+source ~/.bashrc
+
+echo -e "Nodes$B Alice$N and$B Bob$N have been preconfigured, using respectively ports 8331 and 8332"
+echo -e "You can launch node Alice using $B bitcoind-Alice$N and connect with $B bitcoin-cli-Alice$N (connecting via 8321)"
+echo -e "similarly with Bob. Add \`-daemon\` to regain control of the console"
+echo -e "Both should with the original node \"Tux\" launched with $B bitcoind$N "
--- assets/.cryptotux/scripts/lightning.sh
+++ assets/.cryptotux/scripts/lightning.sh
@@ -1,14 +1,109 @@
-#!/bin/bash
-mkdir -p ~/Projects/
-cd ~/Projects/
-git clone https://github.com/bitcoin/bitcoin.git Bitcoin
+### Lightning ###
+# Assumes Bitcoin node and regtest configuration from main script
 
+B="\033[1m"
+N="\033[0m"
+
+## Base configuration based on bitcoin.conf provided with Cryptotux
+echo '
+zmqpubrawblock=tcp://127.0.0.1:28332
+zmqpubrawtx=tcp://127.0.0.1:28333
+addresstype=p2sh-segwit
+' >> ~/.bitcoin/bitcoin.conf
+
+## Install eclair
+# TODO:global function of latest release and include tag
+sudo apt-get install unzip
+eclairVersion="0.4"
+eclairTag="69c538e"
+wget -q https://github.com/ACINQ/eclair/releases/download/v$eclairVersion/eclair-node-$eclairVersion-$eclairTag-bin.zip
+unzip eclair-node-$eclairVersion-$eclairTag-bin.zip
+sudo install -m 0755 -o root -g root -t /usr/local/bin/ eclair-node-$eclairVersion-$eclairTag/bin/*
+sudo install -m 0755 -o root -g root -t /usr/local/lib/ eclair-node-$eclairVersion-$eclairTag/lib/*
+mkdir -p .eclair
+# 
+echo '
+# Bitcoin daemon related configuration
+eclair.chain=regtest
+eclair.bitcoind.rpcport=18443
+eclair.bitcoind.rpcuser=bobby
+eclair.bitcoind.rpcpassword=bricodeur
+eclair.bitcoind.zmqblock="tcp://127.0.0.1:28332"
+eclair.bitcoind.zmqtx="tcp://127.0.0.1:28333"
+
+# Eclair node configuration
+eclair.node-color=69c538
+eclair.api.enabled=true
+eclair.api.password="bricodeur"
+' > ~/.eclair/eclair.conf
+# Create Aliases
+echo 'alias eclair-node="eclair-node.sh";
+' >> ~/.bashrc
+source .bashrc
+echo -e " > Eclair Lightning node installed and available at $B eclair-node$N and $B eclair-cli$N"
+rm eclair-node-$eclairVersion-$eclairTag-bin.zip
+rm -rf eclair-node-$eclairVersion-$eclairTag
+
+## Install lnd 
 go get -u github.com/golang/dep/cmd/dep
 go get -d github.com/lightningnetwork/lnd
 cd $GOPATH/src/github.com/lightningnetwork/lnd
 make && make install
 make check
+mkdir -p ~/.lnd/
+echo '''
+[Bitcoin]
+
+bitcoin.active=1
+bitcoin.regtest=1
+bitcoin.node=bitcoind
+
+[Bitcoind]
+
+bitcoind.rpchost=localhost
+bitcoind.rpcuser=bobby
+bitcoind.rpcpass=bricodeur
+bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332
+bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333
+''' > ~/.lnd/lnd.conf
+echo -e "> lnd Lightning node installed"
+echo -e "   Launch in different terminals$B bitcoind$N ,$B lnd$N, and$B lndcli (create|getinfo|connect...)$N"
+echo -e "   Options are preconfigured in ~/.lnd equivalent to lnd --bitcoin.active --bitcoin.testnet --debuglevel=debug --bitcoin.node=bitcoind --bitcoind.rpcuser=bobby --bitcoind.rpcpass=bricodeur --bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332 --bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333"
+
+
+## Create two nodes for testing, Lena and Naori
+cd  ~/.lnd/
+# Create Lena node
+mkdir -p nodeLena/
+echo '''
+; Lena Node
+[Application Options]
+listen=0.0.0.0:9731
+rpclisten=localhost:10011
+restlisten=0.0.0.0:8081
+''' > nodeLena/lnd.conf
+cat ~/.lnd/lnd.conf >> nodeLena/lnd.conf
+# Create Naori
+mkdir -p ~/nodeNaori/
+cat ~/.lnd/lnd.conf >> nodeNaori/lnd.conf
+echo '''
+; Naori Node
+[Application Options]
+listen=0.0.0.0:9732
+rpclisten=localhost:10012
+restlisten=0.0.0.0:8082
+''' > nodeNaori/lnd.conf
+cat ~/.lnd/lnd.conf >> nodeNaori/lnd.conf
+# Create Aliases
+echo 'export LenaDir="$HOME/.lnd/nodeLena";
+export NaoriDir="$HOME/.lnd/nodeNaori";
+alias lnd-Lena="lnd --lnddir=$LenaDir";
+alias lncli-Lena="lncli -n regtest --lnddir=$LenaDir --rpcserver=localhost:10011";
+alias lnd-Naori="lnd --lnddir=$NaoriDir";
+alias lncli-Naori="lncli -n regtest --lnddir=$NaoriDir --rpcserver=localhost:10012";
+' >> ~/.bashrc
+source ~/.bashrc
+echo -e "> Tutorial installed"
+echo -e "   Two nodes$B Lena$N and$B Naori$N (lnd-Lena, lncli-Lena, lnd-Naori and lncli-Naori)have been preconfigured"
+echo -e "   check .lnd/ folder and this tutorial https://github.com/lightningnetwork/lnd/tree/master/docker"
 # TODO add better configuration and walktrough
-echo -e '\033[1mBitcoin source and Lightning node installed\033[0m'
-echo -e 'launch \033[1mbitcoind \033[0min one terminal and in another one'
-echo -e '\033[1mlnd --bitcoin.active --bitcoin.testnet --debuglevel=debug --bitcoin.node=bitcoind --bitcoind.rpcuser=bobby --bitcoind.rpcpass=bricodeur --bitcoind.zmqpubrawblock=tcp://127.0.0.1:28332 --bitcoind.zmqpubrawtx=tcp://127.0.0.1:28333\033[0m'
